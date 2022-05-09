c8c7eb1fd3a5983e2829b9f33c3676aedb324c9f
Josh Bebbington <josh.bebbington92@gmail.com>
2022-05-09 22:57:07 +0100

    test theme setting

--- install.sh
+++ install.sh
@@ -24,11 +24,11 @@ switch_to_zsh() {
 }
 
 set_zsh_theme() {
-  echo "Setting up the Spaceship theme"
-  sudo apt-get install powerline fonts-powerline -y
-  ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+  # echo "Setting up the Spaceship theme"
+  # sudo apt-get install powerline fonts-powerline -y
+  # ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+  # git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+  # ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 }
 
 if [ "$CODESPACES" = "true" ]; then
