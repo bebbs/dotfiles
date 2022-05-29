796f5cec478ee7c7871d347725b97908135995af
Josh Bebbington <bebbs@users.noreply.github.com>
2022-05-29 18:29:00 +0100

    MacOS uses bsd find, requires argument

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ create_symlinks() {
     script_dir=$(dirname "$(readlink -f "$0")")
 
     # Get a list of all files in this directory that start with a dot.
-    files=$(find -maxdepth 1 -type f -name ".*")
+    files=$(find $script_dir -maxdepth 1 -type f -name ".*")
 
     # Create a symbolic link to each file in the home directory.
     for file in $files; do
